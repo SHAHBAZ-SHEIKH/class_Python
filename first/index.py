c2898772c033efe94e0b25fa87b8e71bcfcdf21d
Shahbaz Ahmed <ashabaz845@gmail.com>
2026-01-02 22:13:57 -0800

    Update Code

---
+++ first/index.py
@@ -0,0 +1,6 @@
+
+# a = 23
+# b = 12
+
+# print("Shahbaz")
+# print(fruits) #apple
---
+++ second/hello.py
@@ -0,0 +1,63 @@
+# fruits = "Appple"
+
+#print((4 > 5 or 7 >8) and (3 >6 and 7 >8 ))
+   #   false  or  false  and 
+
+
+#print(((7 >5 and 8 >5) or (6>3 and 7>5)) and (6>4 or 4>2))
+
+#print(((6>4 or 6<4) and (5<6))  and ((5>6 or 6 >5)) and (4>9))       
+
+
+# num = 5
+# user = "Shahbaz"
+
+# if user == "Shahbaz":
+#     print("True mai chalna hai")
+#     print("Shhabaz")
+
+# else:
+#     print("mai false mai chalta ho")
+
+# age = 18
+# hasCNIC = False
+# if age >=18 and hasCNIC == True:
+#     print("WOw App Vote cast Kr Sakty hai")
+
+# else:
+#     print("Ohh Maaf Kro bhia pehly aap Baary ho kr aao")
+
+
+# percentage = 90
+
+# if percentage >=80:
+#     print("A+ Grade")
+
+# elif percentage >=70 and  percentage<80:
+#     print("A Grade")
+
+# elif percentage>=60 and percentage<70:
+#     print("B Grade")
+
+# elif percentage>=50 and percentage<60:
+#     print("C Grade")
+
+# else:
+#     print("You are Fail")
+
+
+# a = input("please enter value")
+# b = int(a)
+
+# print(type(a))
+
+# print(type(b))
+
+a = input("Please enter value")
+b = int(a)
+print(type(a))
+
+if b == 5:
+    print("Value is 5")
+else:
+    print("Value is not 5")
